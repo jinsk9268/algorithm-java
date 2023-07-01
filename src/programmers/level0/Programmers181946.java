55b992fcc41c963001d1ee6180d7398a67ef751b
jinsk9268 <jinsk9268@gmail.com>
2023-07-01 19:21:26 +0900

    feat: 프로그래머스-문자열 붙여서 출력하기

---
+++ src/programmers/level0/Programmers181946.java
@@ -0,0 +1,20 @@
+package programmers.level0;
+
+import java.util.Scanner;
+
+public class Programmers181946 {
+
+  public void solution() {
+    Scanner sc = new Scanner(System.in);
+    String str1 = sc.next();
+    String str2 = sc.next();
+
+    System.out.println(str1 + str2);
+  }
+
+  public static void main(String[] args) {
+    Programmers181946 t = new Programmers181946();
+
+    t.solution();
+  }
+}
