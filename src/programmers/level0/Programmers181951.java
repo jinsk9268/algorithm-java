f3f51f85bf37ecc316bc55a23957d5e97f3dedf7
jinsk9268 <jinsk9268@gmail.com>
2023-07-01 18:46:39 +0900

    feat: 프로그래머스-a와 b 출력하기

---
+++ src/programmers/level0/Programmers181951.java
@@ -0,0 +1,22 @@
+package programmers.level0;
+
+import java.util.Scanner;
+
+public class Programmers181951 {
+
+  public void solution() {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+  }
+
+  public static void main(String[] args) {
+    Programmers181951 t = new Programmers181951();
+
+    t.solution();
+  }
+}
