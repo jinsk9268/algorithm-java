6bc5d8c598cae9fbf1315c66abb62939cbc41814
jinsk9268 <jinsk9268@gmail.com>
2023-07-01 18:19:22 +0900

    feat: 프로그래머스-문자열 출력하기

---
+++ src/programmers/level0/Programmers181952.java
@@ -0,0 +1,18 @@
+package programmers.level0;
+
+import java.util.Scanner;
+public class Programmers181952 {
+
+  public void solution() {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+
+    System.out.println(str);
+  }
+
+  public static void main(String[] args) {
+    Programmers181952 t = new Programmers181952();
+
+    t.solution();
+  }
+}
